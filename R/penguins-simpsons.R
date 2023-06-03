ae344c06ee916dfe115e2bd22b62129450540155
Michael Friendly <friendly@yorku.ca>
2023-06-02 21:44:28 -0400

    tweak some R files

--- R/penguins-simpsons.R
+++ R/penguins-simpsons.R
@@ -28,12 +28,13 @@ ggplot2::theme_set(theme_bw(base_size = 16))
 #' Ignoring species
 p1 <- ggplot(peng, aes(x=bill_length, y=bill_depth)) +
   geom_point(size=2) +
-  geom_smooth(method="lm", formula=y~x, size=1.5)
+  geom_smooth(method="lm", formula=y~x, linewidth=1.5)
 
 #' Including species
-p2 <- ggplot(peng, aes(x=bill_length, y=bill_depth, color=species, fill=species)) +
+p2 <- ggplot(peng, aes(x=bill_length, y=bill_depth, 
+                       color=species, fill=species)) +
   geom_point(size=2) +
-  geom_smooth(method="lm", formula=y~x, size=1.5) +
+  geom_smooth(method="lm", formula=y~x, linewidth=1.5) +
   theme(legend.position = c(.2, .2))
 
 #' Print side-by-side
